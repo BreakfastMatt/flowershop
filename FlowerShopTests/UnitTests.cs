b5956585c38b8c522baedf3bbef1d874a0cea135
Matthew Lewis <G17L3581@ict.ru.ac.za>
2019-02-22 17:38:24 +0200

    Started test implementation (unfinished)

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using NSubstitute;
 
 namespace Tests
 {
@@ -12,7 +13,22 @@ namespace Tests
         [Test]
         public void Test1()
         {
+            //Arrange
+            IClient cl = Substitute.for<IClient>();//Mock
+            IOrderDAO ord = Substitute.for<IOrderDAO>();//Mock
+            //delivered = false by default
+            IOrder o = new order(ord,cl);//Actual object, made using mock data.
+
+            //Act
+            o.isDelivered = true;
+
+            //Assert
             Assert.Pass();
         }
     }
 }
+/*
+Write a test, using mocks, to check whether the Order class’s Deliver method calls
+the SetDelivered method in IOrderDAO. Use comments in the test to specifically
+show where your ARRANGE, ACT, and ASSERT sections are. 
+*/
